473e69c744c852a0711455223455d254f29362cb
oliviajools <oliviajools@icloud.com>
2026-06-07 13:55:54 +0200

    Create simple admin page for blog posts

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -45,6 +45,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../app/admin/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/admin">> = Specific
+  const handler = {} as typeof import("../../app/admin/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../app/company/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/company">> = Specific
